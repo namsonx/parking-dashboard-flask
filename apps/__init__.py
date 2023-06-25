5447c0ae76d012169bf26c343c7b0a2b18072b30
mas2hc <son.maidinhnam@vn.bosch.com>
2023-06-25 22:18:52 +0700

    First initialization

--- apps/__init__.py
+++ apps/__init__.py
@@ -7,7 +7,8 @@ from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from importlib import import_module
-
+import pymysql
+pymysql.install_as_MySQLdb()
 
 db = SQLAlchemy()
 login_manager = LoginManager()
--- apps/config.py
+++ apps/config.py
@@ -13,8 +13,7 @@ class Config(object):
     # Set up the App SECRET_KEY
     SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_007')
 
-    # This will create a file in <app> FOLDER
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'db.sqlite3')
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:Son123456@localhost/cloudparking'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
@@ -28,12 +27,12 @@ class ProductionConfig(Config):
 
     # PostgreSQL database
     SQLALCHEMY_DATABASE_URI = '{}://{}:{}@{}:{}/{}'.format(
-        config('DB_ENGINE', default='postgresql'),
-        config('DB_USERNAME', default='appseed'),
-        config('DB_PASS', default='pass'),
+        config('DB_ENGINE', default='mysql'),
+        config('DB_USERNAME', default='root'),
+        config('DB_PASS', default='Son123456'),
         config('DB_HOST', default='localhost'),
-        config('DB_PORT', default=5432),
-        config('DB_NAME', default='appseed-flask')
+        config('DB_PORT', default=3306),
+        config('DB_NAME', default='cloudparking')
     )
 
 
